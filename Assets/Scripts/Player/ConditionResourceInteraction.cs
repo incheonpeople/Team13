42f831d614307db113058972a7b0b7878712b8a2
parktkdrb <qkrtkdrb05@naver.com>
2024-11-01 15:15:59 +0900

    [Update] 상호작용 가능한 오브젝트 체크(기능 작동 안하고 체크만.)

--- Assets/Scripts/Player/ConditionResourceInteraction.cs
+++ Assets/Scripts/Player/ConditionResourceInteraction.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class ConditionResourceInteraction : MonoBehaviour
 {
-    [SerializeField]LayerMask layer;
+    [SerializeField] private LayerMask layerMask;
     [SerializeField] private PlayerConditions conditions;
+    [SerializeField] private float maxCheckDistance;
+    private Camera camera;
     // Start is called before the first frame update
     void Start()
     {
         conditions = GetComponent<PlayerConditions>();
+        camera = Camera.main;
     }
-
+    private void Update()
+    {
+        ResourceInteraction();
+    }
+    public void ResourceInteraction()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2)); 
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+        {
+            Debug.Log("¹° ¹ß°ß");
+        }
+    }
 }
